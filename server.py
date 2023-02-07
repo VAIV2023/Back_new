4b1223e9dc9d236b871ee0740cb0e32450944207
sera2002 <ouioui2@g.skku.edu>
2023-02-07 02:21:18 +0000

    cache 사용하도록 수정 (flask-caching)

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
-from werkzeug.contrib.cache import GAEMemcachedCache
+from flask_caching import Cache
 
 from datetime import datetime, timedelta
 import exchange_calendars as ecals 
@@ -11,7 +11,8 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 CORS(app)
 
-cache = GAEMemcachedCache()
+cache = Cache(config={'CACHE_TYPE': 'simple'})
+cache.init_app(app)
 
 client = MongoClient('mongodb://vaivwinter:vaivwinter2023!@43.201.8.26', 27017)
 
@@ -55,38 +56,36 @@ def login():
     
 
 @app.route("/showtoppick", methods=['GET', 'POST'])
+@cache.cached(timeout=600)
 def toppick_show():
     #option = request.json
     #print(option)
-    res_dict = cache.get('toppick')
+    
+    # 오늘 날짜 그대로 넣으면 안되는구나..
+    # 오늘이 개장일이면 오늘 날짜 그대로 입력
+    # 오늘이 폐장일이면 ?? 다음에 올 개장일로 입력해야 됨.
+    XKRX = ecals.get_calendar("XKRX") # 한국 코드
+    date = datetime.today().strftime("%Y-%m-%d")
+    if not XKRX.is_session(date):
+        next_date = XKRX.next_open(pd.Timestamp.today()) # 다음 개장일은 언제인지 확인
+        date = next_date.strftime("%Y-%m-%d")
+    
+    # temp code
+    date = "2023-02-06"
 
-    if res_dict == None:
-        # 오늘 날짜 그대로 넣으면 안되는구나..
-        # 오늘이 개장일이면 오늘 날짜 그대로 입력
-        # 오늘이 폐장일이면 ?? 다음에 올 개장일로 입력해야 됨.
-        XKRX = ecals.get_calendar("XKRX") # 한국 코드
-        date = datetime.today().strftime("%Y-%m-%d")
-        if not XKRX.is_session(date):
-            next_date = XKRX.next_open(pd.Timestamp.today()) # 다음 개장일은 언제인지 확인
-            date = next_date.strftime("%Y-%m-%d")
-        
-        res_dict = {'KOSPI' : [], 'KOSDAQ' : []}
-        market_list = ['KOSPI', 'KOSDAQ']
-        # static/toppick_csv/{market}/날짜.txt 에 저장되어있는 toppick 종목
-        # line by line으로 읽어와서 리턴
-        for market in market_list:
-            f = open(f"/home/ubuntu/Back_new/static/toppick/{market}/{date}.txt", "r")
-            lst = []
-            while True:
-                line = f.readline().strip()
-                if not line: break
-                lst.append(line)
+    res_dict = {'KOSPI' : [], 'KOSDAQ' : []}
+    market_list = ['KOSPI', 'KOSDAQ']
+    # static/toppick_csv/{market}/날짜.txt 에 저장되어있는 toppick 종목
+    # line by line으로 읽어와서 리턴
+    for market in market_list:
+        f = open(f"/home/ubuntu/Back_new/static/toppick/{market}/{date}.txt", "r")
+        lst = []
+        while True:
+            line = f.readline().strip()
+            if not line: break
+            lst.append(line)
 
-            res_dict[market] = lst
-
-        cache.set('toppick', res_dict)
-    else:
-        res_dict = cache.get(toppick)
+        res_dict[market] = lst
 
     print(res_dict)
     return jsonify(res_dict)
