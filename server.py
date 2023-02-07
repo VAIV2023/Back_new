2c81736441b2c0f9af9fbeb056580fee9a33b93e
sera2002 <ouioui2@g.skku.edu>
2023-02-07 01:32:20 +0000

    cache 사용

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
+from werkzeug.contrib.cache import GAEMemcachedCache
 
 from datetime import datetime, timedelta
 import exchange_calendars as ecals 
@@ -9,6 +10,8 @@ import pandas as pd
 app = Flask(__name__)
 app.config['DEBUG'] = True
 CORS(app)
+
+cache = GAEMemcachedCache()
 
 client = MongoClient('mongodb://vaivwinter:vaivwinter2023!@43.201.8.26', 27017)
 
@@ -25,58 +28,66 @@ def login():
     # success(1) : 로그인 성공
     # success(0) : 계정 등록
     # success(-1) : 에러
-    option = request.json
-    id = option['id']
-    success = -1
     res_dict = {}
+    success = -1
 
-    db = client.portfolio
-    for d in db['user'].find():
-        if d['user_id'] == id:
-            success = 1
-    
-    if sucess == -1:
-        # DB에 계정 새로 등록
-        print("=== Resigter ===")
-        print(f"id : {id}")
-        user_info = {
-            'user_id': id,
-            'accounts': {}
-        }
-        db.user.insert_one(user_info)
-    
+    if request.method == 'POST':
+        option = request.json
+        id = option['id']
+
+        db = client.portfolio
+        for d in db['user'].find():
+            if d['user_id'] == id:
+                success = 1
+        
+        if sucess == -1:
+            # DB에 계정 새로 등록
+            print("=== Resigter ===")
+            print(f"id : {id}")
+            user_info = {
+                'user_id': id,
+                'accounts': {}
+            }
+            db.user.insert_one(user_info)
+        
     res_dict['success'] = success
     return res_dict
     
 
 @app.route("/showtoppick", methods=['GET', 'POST'])
 def toppick_show():
-    option = request.json
-    print(option)
+    #option = request.json
+    #print(option)
+    res_dict = cache.get('toppick')
 
-    # 오늘 날짜 그대로 넣으면 안되는구나..
-    # 오늘이 개장일이면 오늘 날짜 그대로 입력
-    # 오늘이 폐장일이면 ?? 다음에 올 개장일로 입력해야 됨.
-    XKRX = ecals.get_calendar("XKRX") # 한국 코드
-    date = datetime.today().strftime("%Y-%m-%d")
-    if not XKRX.is_session(date):
-        next_date = XKRX.next_open(pd.Timestamp.today()) # 다음 개장일은 언제인지 확인
-        date = next_date.strftime("%Y-%m-%d")
-    
-    res_dict = {'KOSPI' : [], 'KOSDAQ' : []}
-    market_list = ['KOSPI', 'KOSDAQ']
-    # static/toppick_csv/{market}/날짜.txt 에 저장되어있는 toppick 종목
-    # line by line으로 읽어와서 리턴
-    for market in market_list:
-        f = open(f"/home/ubuntu/Back_new/static/toppick/{market}/{date}.txt", "r")
-        lst = []
-        while True:
-            line = f.readline().strip()
-            if not line: break
-            lst.append(line)
+    if res_dict == None:
+        # 오늘 날짜 그대로 넣으면 안되는구나..
+        # 오늘이 개장일이면 오늘 날짜 그대로 입력
+        # 오늘이 폐장일이면 ?? 다음에 올 개장일로 입력해야 됨.
+        XKRX = ecals.get_calendar("XKRX") # 한국 코드
+        date = datetime.today().strftime("%Y-%m-%d")
+        if not XKRX.is_session(date):
+            next_date = XKRX.next_open(pd.Timestamp.today()) # 다음 개장일은 언제인지 확인
+            date = next_date.strftime("%Y-%m-%d")
+        
+        res_dict = {'KOSPI' : [], 'KOSDAQ' : []}
+        market_list = ['KOSPI', 'KOSDAQ']
+        # static/toppick_csv/{market}/날짜.txt 에 저장되어있는 toppick 종목
+        # line by line으로 읽어와서 리턴
+        for market in market_list:
+            f = open(f"/home/ubuntu/Back_new/static/toppick/{market}/{date}.txt", "r")
+            lst = []
+            while True:
+                line = f.readline().strip()
+                if not line: break
+                lst.append(line)
 
-        res_dict[market] = lst
-    
+            res_dict[market] = lst
+
+        cache.set('toppick', res_dict)
+    else:
+        res_dict = cache.get(toppick)
+
     print(res_dict)
     return jsonify(res_dict)
 
